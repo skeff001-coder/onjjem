edf1888494d38916e985ebbdeea1a560e95b500f
skeff001-coder <skeff001@yahoo.com>
2026-08-20 19:52:17 +0100

    Update index.tsx

--- artifacts/owens-photofix/app/index.tsx
+++ artifacts/owens-photofix/app/index.tsx
@@ -240,7 +240,6 @@ activeOpacity={0.88}
 
 <Text style={s.mugTitle}>The Magic Photo Mug</Text>
 
-<Image source={require("@/assets/magic-mug-reveal.png")} style={s.mugImage} />
 
 <Text style={s.mugDesc}>
 
