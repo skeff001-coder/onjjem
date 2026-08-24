ade8586f4f1029ada9776699f5d8f2f31a2a282e
skeff001-coder <skeff001@yahoo.com>
2026-08-24 01:55:20 +0100

    Remove KeyboardProvider from layout

--- artifacts/canine-encyclopedia/app/_layout.tsx
+++ artifacts/canine-encyclopedia/app/_layout.tsx
@@ -11,7 +11,6 @@ import * as SplashScreen from "expo-splash-screen";
 import React, { useEffect } from "react";
 import { Alert } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { KeyboardProvider } from "react-native-keyboard-controller";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import { ErrorBoundary } from "@/components/ErrorBoundary";
@@ -69,13 +68,11 @@ export default function RootLayout() {
       <ErrorBoundary>
         <QueryClientProvider client={queryClient}>
           <GestureHandlerRootView style={{ flex: 1 }}>
-            <KeyboardProvider>
-              <AppProvider>
-                <SubscriptionProvider>
-                  <RootLayoutNav />
-                </SubscriptionProvider>
-              </AppProvider>
-            </KeyboardProvider>
+            <AppProvider>
+              <SubscriptionProvider>
+                <RootLayoutNav />
+              </SubscriptionProvider>
+            </AppProvider>
           </GestureHandlerRootView>
         </QueryClientProvider>
       </ErrorBoundary>
