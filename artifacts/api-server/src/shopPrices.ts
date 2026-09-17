4e92fbffc1cc3676b2b27c1927522e9e22a2545c
skeff001-coder <skeff001@yahoo.com>
2026-09-17 04:09:02 +0100

    Update shopPrices.ts

--- artifacts/api-server/src/shopPrices.ts
+++ artifacts/api-server/src/shopPrices.ts
@@ -2,37 +2,44 @@
 // 30 SKUs: 5 sizes × 6 colors
 // Price: £16.99 (1699 pence)
 
-"TEE-STTK184-3Y-WHITE": { name: "Cartoon Kids' Premium T-Shirt (3-4 Years, White)", pricePence: 1699 },
-"TEE-STTK184-3Y-BLACK": { name: "Cartoon Kids' Premium T-Shirt (3-4 Years, Black)", pricePence: 1699 },
-"TEE-STTK184-3Y-GREY": { name: "Cartoon Kids' Premium T-Shirt (3-4 Years, Heather Grey)", pricePence: 1699 },
-"TEE-STTK184-3Y-RED": { name: "Cartoon Kids' Premium T-Shirt (3-4 Years, Fire Red)", pricePence: 1699 },
-"TEE-STTK184-3Y-ROYALBLUE": { name: "Cartoon Kids' Premium T-Shirt (3-4 Years, Royal Blue)", pricePence: 1699 },
-"TEE-STTK184-3Y-PINK": { name: "Cartoon Kids' Premium T-Shirt (3-4 Years, Cotton Pink)", pricePence: 1699 },
+export const SHOP_SKU_PRICES = {
+  // 3-4 Years
+  "TEE-STTK184-3Y-WHITE": { name: "Cartoon Kids' Premium T-Shirt (3-4 Years, White)", pricePence: 1699 },
+  "TEE-STTK184-3Y-BLACK": { name: "Cartoon Kids' Premium T-Shirt (3-4 Years, Black)", pricePence: 1699 },
+  "TEE-STTK184-3Y-GREY": { name: "Cartoon Kids' Premium T-Shirt (3-4 Years, Heather Grey)", pricePence: 1699 },
+  "TEE-STTK184-3Y-RED": { name: "Cartoon Kids' Premium T-Shirt (3-4 Years, Fire Red)", pricePence: 1699 },
+  "TEE-STTK184-3Y-ROYALBLUE": { name: "Cartoon Kids' Premium T-Shirt (3-4 Years, Royal Blue)", pricePence: 1699 },
+  "TEE-STTK184-3Y-PINK": { name: "Cartoon Kids' Premium T-Shirt (3-4 Years, Cotton Pink)", pricePence: 1699 },
 
-"TEE-STTK184-5Y-WHITE": { name: "Cartoon Kids' Premium T-Shirt (5-6 Years, White)", pricePence: 1699 },
-"TEE-STTK184-5Y-BLACK": { name: "Cartoon Kids' Premium T-Shirt (5-6 Years, Black)", pricePence: 1699 },
-"TEE-STTK184-5Y-GREY": { name: "Cartoon Kids' Premium T-Shirt (5-6 Years, Heather Grey)", pricePence: 1699 },
-"TEE-STTK184-5Y-RED": { name: "Cartoon Kids' Premium T-Shirt (5-6 Years, Fire Red)", pricePence: 1699 },
-"TEE-STTK184-5Y-ROYALBLUE": { name: "Cartoon Kids' Premium T-Shirt (5-6 Years, Royal Blue)", pricePence: 1699 },
-"TEE-STTK184-5Y-PINK": { name: "Cartoon Kids' Premium T-Shirt (5-6 Years, Cotton Pink)", pricePence: 1699 },
+  // 5-6 Years
+  "TEE-STTK184-5Y-WHITE": { name: "Cartoon Kids' Premium T-Shirt (5-6 Years, White)", pricePence: 1699 },
+  "TEE-STTK184-5Y-BLACK": { name: "Cartoon Kids' Premium T-Shirt (5-6 Years, Black)", pricePence: 1699 },
+  "TEE-STTK184-5Y-GREY": { name: "Cartoon Kids' Premium T-Shirt (5-6 Years, Heather Grey)", pricePence: 1699 },
+  "TEE-STTK184-5Y-RED": { name: "Cartoon Kids' Premium T-Shirt (5-6 Years, Fire Red)", pricePence: 1699 },
+  "TEE-STTK184-5Y-ROYALBLUE": { name: "Cartoon Kids' Premium T-Shirt (5-6 Years, Royal Blue)", pricePence: 1699 },
+  "TEE-STTK184-5Y-PINK": { name: "Cartoon Kids' Premium T-Shirt (5-6 Years, Cotton Pink)", pricePence: 1699 },
 
-"TEE-STTK184-7Y-WHITE": { name: "Cartoon Kids' Premium T-Shirt (7-8 Years, White)", pricePence: 1699 },
-"TEE-STTK184-7Y-BLACK": { name: "Cartoon Kids' Premium T-Shirt (7-8 Years, Black)", pricePence: 1699 },
-"TEE-STTK184-7Y-GREY": { name: "Cartoon Kids' Premium T-Shirt (7-8 Years, Heather Grey)", pricePence: 1699 },
-"TEE-STTK184-7Y-RED": { name: "Cartoon Kids' Premium T-Shirt (7-8 Years, Fire Red)", pricePence: 1699 },
-"TEE-STTK184-7Y-ROYALBLUE": { name: "Cartoon Kids' Premium T-Shirt (7-8 Years, Royal Blue)", pricePence: 1699 },
-"TEE-STTK184-7Y-PINK": { name: "Cartoon Kids' Premium T-Shirt (7-8 Years, Cotton Pink)", pricePence: 1699 },
+  // 7-8 Years
+  "TEE-STTK184-7Y-WHITE": { name: "Cartoon Kids' Premium T-Shirt (7-8 Years, White)", pricePence: 1699 },
+  "TEE-STTK184-7Y-BLACK": { name: "Cartoon Kids' Premium T-Shirt (7-8 Years, Black)", pricePence: 1699 },
+  "TEE-STTK184-7Y-GREY": { name: "Cartoon Kids' Premium T-Shirt (7-8 Years, Heather Grey)", pricePence: 1699 },
+  "TEE-STTK184-7Y-RED": { name: "Cartoon Kids' Premium T-Shirt (7-8 Years, Fire Red)", pricePence: 1699 },
+  "TEE-STTK184-7Y-ROYALBLUE": { name: "Cartoon Kids' Premium T-Shirt (7-8 Years, Royal Blue)", pricePence: 1699 },
+  "TEE-STTK184-7Y-PINK": { name: "Cartoon Kids' Premium T-Shirt (7-8 Years, Cotton Pink)", pricePence: 1699 },
 
-"TEE-STTK184-9Y-WHITE": { name: "Cartoon Kids' Premium T-Shirt (9-11 Years, White)", pricePence: 1699 },
-"TEE-STTK184-9Y-BLACK": { name: "Cartoon Kids' Premium T-Shirt (9-11 Years, Black)", pricePence: 1699 },
-"TEE-STTK184-9Y-GREY": { name: "Cartoon Kids' Premium T-Shirt (9-11 Years, Heather Grey)", pricePence: 1699 },
-"TEE-STTK184-9Y-RED": { name: "Cartoon Kids' Premium T-Shirt (9-11 Years, Fire Red)", pricePence: 1699 },
-"TEE-STTK184-9Y-ROYALBLUE": { name: "Cartoon Kids' Premium T-Shirt (9-11 Years, Royal Blue)", pricePence: 1699 },
-"TEE-STTK184-9Y-PINK": { name: "Cartoon Kids' Premium T-Shirt (9-11 Years, Cotton Pink)", pricePence: 1699 },
+  // 9-11 Years
+  "TEE-STTK184-9Y-WHITE": { name: "Cartoon Kids' Premium T-Shirt (9-11 Years, White)", pricePence: 1699 },
+  "TEE-STTK184-9Y-BLACK": { name: "Cartoon Kids' Premium T-Shirt (9-11 Years, Black)", pricePence: 1699 },
+  "TEE-STTK184-9Y-GREY": { name: "Cartoon Kids' Premium T-Shirt (9-11 Years, Heather Grey)", pricePence: 1699 },
+  "TEE-STTK184-9Y-RED": { name: "Cartoon Kids' Premium T-Shirt (9-11 Years, Fire Red)", pricePence: 1699 },
+  "TEE-STTK184-9Y-ROYALBLUE": { name: "Cartoon Kids' Premium T-Shirt (9-11 Years, Royal Blue)", pricePence: 1699 },
+  "TEE-STTK184-9Y-PINK": { name: "Cartoon Kids' Premium T-Shirt (9-11 Years, Cotton Pink)", pricePence: 1699 },
 
-"TEE-STTK184-12Y-WHITE": { name: "Cartoon Kids' Premium T-Shirt (12-14 Years, White)", pricePence: 1699 },
-"TEE-STTK184-12Y-BLACK": { name: "Cartoon Kids' Premium T-Shirt (12-14 Years, Black)", pricePence: 1699 },
-"TEE-STTK184-12Y-GREY": { name: "Cartoon Kids' Premium T-Shirt (12-14 Years, Heather Grey)", pricePence: 1699 },
-"TEE-STTK184-12Y-RED": { name: "Cartoon Kids' Premium T-Shirt (12-14 Years, Fire Red)", pricePence: 1699 },
-"TEE-STTK184-12Y-ROYALBLUE": { name: "Cartoon Kids' Premium T-Shirt (12-14 Years, Royal Blue)", pricePence: 1699 },
-"TEE-STTK184-12Y-PINK": { name: "Cartoon Kids' Premium T-Shirt (12-14 Years, Cotton Pink)", pricePence: 1699 },
+  // 12-14 Years
+  "TEE-STTK184-12Y-WHITE": { name: "Cartoon Kids' Premium T-Shirt (12-14 Years, White)", pricePence: 1699 },
+  "TEE-STTK184-12Y-BLACK": { name: "Cartoon Kids' Premium T-Shirt (12-14 Years, Black)", pricePence: 1699 },
+  "TEE-STTK184-12Y-GREY": { name: "Cartoon Kids' Premium T-Shirt (12-14 Years, Heather Grey)", pricePence: 1699 },
+  "TEE-STTK184-12Y-RED": { name: "Cartoon Kids' Premium T-Shirt (12-14 Years, Fire Red)", pricePence: 1699 },
+  "TEE-STTK184-12Y-ROYALBLUE": { name: "Cartoon Kids' Premium T-Shirt (12-14 Years, Royal Blue)", pricePence: 1699 },
+  "TEE-STTK184-12Y-PINK": { name: "Cartoon Kids' Premium T-Shirt (12-14 Years, Cotton Pink)", pricePence: 1699 },
+};
